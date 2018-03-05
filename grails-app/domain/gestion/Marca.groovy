8b06a52fbfd245b4e49f102afaa16d5a1415c753
Angel Careaga <acareagag@gmail.com>
2018-03-05 10:47:42 -0600

    Secuencia en Marca
    Se agrego secuencia en Marca.

--- grails-app/domain/gestion/Marca.groovy
+++ grails-app/domain/gestion/Marca.groovy
@@ -3,4 +3,8 @@ package gestion
 class Marca {
     String nombreMarca
     static hasMany = [automoviles: Automovil]
+
+    static mapping= {
+        id generate  : 'sequence', column:'id_marca', params:[sequence:'marcas']
+    }
 }
